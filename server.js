185482953f7e8da6ce6bcfb1b18184ee38ce7ea3
donkeykong91 <darylnator@gmail.com>
2018-09-22 22:07:40 -0700

    Added a new link tag to header to refer to the style.css file created from sass middleware in server.js

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ import fs from "fs";
 
 import express from "express";
 
+import sassMiddleware from "node-sass-middleware";
+
+import path from "path";
+
 const server = express();
 
 
@@ -26,6 +30,22 @@ server.get("/", function (request, response) {
 server.use(express.static("public"));
 
 server.use("/api", apiRouter);
+
+server.use(
+
+  sassMiddleware(
+
+    {
+
+      src: path.join(__dirname, "sass"),
+
+      dest: path.join(__dirname, "public")
+
+    }
+
+  )
+
+);
 
 
 server.listen(config.port, function () {
